3ab6c302ac5d451dffb9f657a3af51e09d72aa98
mikelangston <mike@mikelangston.me>
2018-01-07 21:34:03 -0500

    added idea library page

--- src/assets/dockingdrawer.js
+++ src/assets/dockingdrawer.js
@@ -2,6 +2,14 @@ function new_window(){
   $('a[href^="http"]').not('a[href^="'+$(location).attr('hostname')+'"]').attr('target', '_blank');
 }
 
+function photo_gallery(){
+
+}
+
 $(document).ready(function(){
   new_window();
+  lightbox.option({
+    'wrapAround': true,
+    'alwaysShowNavOnTouchDevices': true
+  });
 });
