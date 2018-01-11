03bc6c8e2f83745300f423a34197affbeb0d75c8
mikelangston <mike@mikelangston.me>
2018-01-10 20:55:09 -0500

    update to contact page

--- src/assets/dockingdrawer.js
+++ src/assets/dockingdrawer.js
@@ -2,12 +2,23 @@ function new_window(){
   $('a[href^="http"]').not('a[href^="'+$(location).attr('hostname')+'"]').attr('target', '_blank');
 }
 
-function photo_gallery(){
-
+function exchange_return(){
+  $('.exchange-btn').click(function(e){
+    e.preventDefault();
+    $('.overlay.exchange-content').show();
+  });
+  $('.return-btn').click(function(e){
+    e.preventDefault();
+    $('.overlay.return-content').show();
+  });
+  $('.close').click(function(){
+    $('.overlay').hide();
+  });
 }
 
 $(document).ready(function(){
   new_window();
+  exchange_return();
   lightbox.option({
     'wrapAround': true,
     'alwaysShowNavOnTouchDevices': true
