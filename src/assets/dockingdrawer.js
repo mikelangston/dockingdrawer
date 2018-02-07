d2cfd1df3f6538d6c58f181a2de487ad20f0dabe
mikelangston <mike@mikelangston.me>
2018-02-06 22:01:34 -0500

    build changes

--- src/assets/dockingdrawer.js
+++ src/assets/dockingdrawer.js
@@ -2,6 +2,10 @@ function new_window(){
   $('a[href^="http"]').not('a[href^="'+$(location).attr('hostname')+'"]').attr('target', '_blank');
 }
 
+function amazon_store(){
+  $('a[href="https://www.amazon.com/stores/page/CB80812F-6178-4DEB-8B7C-005E0D8ECF96"]').attr('target', '_blank');
+}
+
 function exchange_return(){
   $('.exchange-btn').click(function(e){
     e.preventDefault();
@@ -19,6 +23,7 @@ function exchange_return(){
 
 $(document).ready(function(){
   //new_window();
+  amazon_store();
   exchange_return();
   lightbox.option({
     'wrapAround': true,
